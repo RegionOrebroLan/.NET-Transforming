4a41d20a433a484f7504092ecc54fbf2a4157ea0
HansKindberg <hans.kindberg@gmail.com>
2024-03-25 21:06:14 +0100

    VSTest -> Xunit (some tests)

--- Tests/Unit-tests/IO/Extensions/PathExtensionTest.cs
+++ Tests/Unit-tests/IO/Extensions/PathExtensionTest.cs
@@ -1,15 +1,14 @@
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RegionOrebroLan.Transforming.IO.Extensions;
+using Xunit;
 
 namespace UnitTests.IO.Extensions
 {
-	[TestClass]
 	public class PathExtensionTest
 	{
 		#region Methods
 
-		[TestMethod]
+		[Fact]
 		public async Task EnsureTrailingDirectorySeparator_IfThePathDoesNotEndWithADirectorySeparator_ShouldReturnAPathWithATrailingDirectorySeparator()
 		{
 			await Task.CompletedTask;
@@ -18,46 +17,46 @@ namespace UnitTests.IO.Extensions
 
 			var path = directoryName;
 			var expected = $"{directoryName}{(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"\" : "/")}";
-			Assert.AreEqual(expected, PathExtension.EnsureTrailingDirectorySeparator(path));
+			Assert.Equal(expected, PathExtension.EnsureTrailingDirectorySeparator(path));
 
 			path = $"{directoryName}/";
 			expected = path;
-			Assert.AreEqual(expected, PathExtension.EnsureTrailingDirectorySeparator(path));
+			Assert.Equal(expected, PathExtension.EnsureTrailingDirectorySeparator(path));
 
 			path = $"{directoryName}\\";
 			expected = $"{directoryName}{(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"\" : @"\/")}";
-			Assert.AreEqual(expected, PathExtension.EnsureTrailingDirectorySeparator(path));
+			Assert.Equal(expected, PathExtension.EnsureTrailingDirectorySeparator(path));
 		}
 
-		[TestMethod]
+		[Fact]
 		public async Task IsPathFullyQualified_Test()
 		{
 			await Task.CompletedTask;
 
-			Assert.IsFalse(PathExtension.IsPathFullyQualified("test"));
-			Assert.IsFalse(PathExtension.IsPathFullyQualified("test/"));
-			Assert.IsFalse(PathExtension.IsPathFullyQualified("test.txt"));
+			Assert.False(PathExtension.IsPathFullyQualified("test"));
+			Assert.False(PathExtension.IsPathFullyQualified("test/"));
+			Assert.False(PathExtension.IsPathFullyQualified("test.txt"));
 
 			if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 			{
-				Assert.IsFalse(PathExtension.IsPathFullyQualified("/test"));
-				Assert.IsFalse(PathExtension.IsPathFullyQualified("/test/"));
-				Assert.IsFalse(PathExtension.IsPathFullyQualified("/test.txt"));
-				Assert.IsFalse(PathExtension.IsPathFullyQualified(@"\test"));
-				Assert.IsFalse(PathExtension.IsPathFullyQualified(@"\test\"));
-				Assert.IsFalse(PathExtension.IsPathFullyQualified(@"\test.txt"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified("C:/test"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified("C:/test/"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified("C:/test.txt"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified(@"C:\test"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified(@"C:\test\"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified(@"C:\test.txt"));
+				Assert.False(PathExtension.IsPathFullyQualified("/test"));
+				Assert.False(PathExtension.IsPathFullyQualified("/test/"));
+				Assert.False(PathExtension.IsPathFullyQualified("/test.txt"));
+				Assert.False(PathExtension.IsPathFullyQualified(@"\test"));
+				Assert.False(PathExtension.IsPathFullyQualified(@"\test\"));
+				Assert.False(PathExtension.IsPathFullyQualified(@"\test.txt"));
+				Assert.True(PathExtension.IsPathFullyQualified("C:/test"));
+				Assert.True(PathExtension.IsPathFullyQualified("C:/test/"));
+				Assert.True(PathExtension.IsPathFullyQualified("C:/test.txt"));
+				Assert.True(PathExtension.IsPathFullyQualified(@"C:\test"));
+				Assert.True(PathExtension.IsPathFullyQualified(@"C:\test\"));
+				Assert.True(PathExtension.IsPathFullyQualified(@"C:\test.txt"));
 			}
 			else
 			{
-				Assert.IsTrue(PathExtension.IsPathFullyQualified("/test"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified("/test/"));
-				Assert.IsTrue(PathExtension.IsPathFullyQualified("/test.txt"));
+				Assert.True(PathExtension.IsPathFullyQualified("/test"));
+				Assert.True(PathExtension.IsPathFullyQualified("/test/"));
+				Assert.True(PathExtension.IsPathFullyQualified("/test.txt"));
 			}
 		}
 
