ed876eaed2dbcafce3f6c6f674c722f78fbd5ff2
HansKindberg <hans.kindberg@gmail.com>
2024-03-28 13:52:18 +0100

    Bom-tests

--- Tests/Integration-tests/JsonTransformerTest.cs
+++ Tests/Integration-tests/JsonTransformerTest.cs
@@ -2,6 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using IntegrationTests.Fixtures;
 using IntegrationTests.Helpers;
+using RegionOrebroLan.Transforming.Configuration;
 using RegionOrebroLan.Transforming.IO.Extensions;
 using Xunit;
 
@@ -18,6 +19,26 @@ namespace IntegrationTests
 
 		#region Methods
 
+		private string GetBomCrlfResourcePath(params string[] paths)
+		{
+			return this._fixture.GetResourcePath(ResolvePaths(new[] { "bom", "crlf" }.Concat(paths).ToArray()));
+		}
+
+		//private string GetBomLfResourcePath(params string[] paths)
+		//{
+		//	return this._fixture.GetResourcePath(ResolvePaths(new[] { "bom", "lf" }.Concat(paths).ToArray()));
+		//}
+
+		private string GetNoBomCrlfResourcePath(params string[] paths)
+		{
+			return this._fixture.GetResourcePath(ResolvePaths(new[] { "no-bom", "crlf" }.Concat(paths).ToArray()));
+		}
+
+		//private string GetNoBomLfResourcePath(params string[] paths)
+		//{
+		//	return this._fixture.GetResourcePath(ResolvePaths(new[] { "no-bom", "lf" }.Concat(paths).ToArray()));
+		//}
+
 		private string GetOutputPath(params string[] paths)
 		{
 			return this._fixture.GetOutputPath(ResolvePaths(paths));
@@ -26,6 +47,11 @@ namespace IntegrationTests
 		private string GetResourcePath(params string[] paths)
 		{
 			return this._fixture.GetResourcePath(ResolvePaths(paths));
+		}
+
+		private static string GetUniqueSuffix()
+		{
+			return $"-{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
 		}
 
 		private static string[] ResolvePaths(params string[] paths)
@@ -87,11 +113,58 @@ namespace IntegrationTests
 		}
 
 		[Fact]
+		public void Transform_IfTheSourceHasABom_And_IfTheOptionsAreDefault_ShouldResultInADestinationWithABomOnWindows_And_ShouldResultInADestinationWithNoBomOnNonWindows()
+		{
+			var destination = this.GetOutputPath($"appsettings{GetUniqueSuffix()}.json");
+			var source = this.GetBomCrlfResourcePath("appsettings.json");
+			var transformation = this.GetBomCrlfResourcePath("appsettings.Transformation.json");
+
+			this._fixture.JsonTransformer.Transform(destination, source, transformation);
+
+			using(var streamReader = new StreamReader(destination, true))
+			{
+				if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+					Assert.True(streamReader.HasByteOrderMark());
+				else
+					Assert.False(streamReader.HasByteOrderMark());
+			}
+		}
+
+		[Fact]
+		public void Transform_IfTheSourceHasABom_And_IfTheOptionsAreSetToAvoidBom_ShouldResultInADestinationWithNoBom()
+		{
+			var destination = this.GetOutputPath($"appsettings{GetUniqueSuffix()}.json");
+			var source = this.GetBomCrlfResourcePath("appsettings.json");
+			var transformation = this.GetBomCrlfResourcePath("appsettings.Transformation.json");
+
+			this._fixture.JsonTransformer.Transform(destination, source, transformation, new FileTransformingOptions { AvoidByteOrderMark = true });
+
+			using(var streamReader = new StreamReader(destination, true))
+			{
+				Assert.False(streamReader.HasByteOrderMark());
+			}
+		}
+
+		[Fact]
+		public void Transform_IfTheSourceHasNoBom_ShouldResultInADestinationWithNoBom()
+		{
+			var destination = this.GetOutputPath($"appsettings{GetUniqueSuffix()}.json");
+			var source = this.GetNoBomCrlfResourcePath("appsettings.json");
+			var transformation = this.GetBomCrlfResourcePath("appsettings.Transformation.json");
+
+			this._fixture.JsonTransformer.Transform(destination, source, transformation);
+
+			using(var streamReader = new StreamReader(destination, true))
+			{
+				Assert.False(streamReader.HasByteOrderMark());
+			}
+		}
+
+		[Fact]
 		public void Transform_IfTheTransformationContentIsEmpty_ShouldTransformCorrectly()
 		{
-			const string fileName = "appsettings.json";
-			var destination = this.GetOutputPath(fileName);
-			var source = this.GetResourcePath(fileName);
+			var destination = this.GetOutputPath($"appsettings{GetUniqueSuffix()}.json");
+			var source = this.GetResourcePath("appsettings.json");
 			var transformation = this.GetResourcePath("appsettings.No-Transformation.json");
 
 			this._fixture.JsonTransformer.Transform(destination, source, transformation);
@@ -105,9 +178,8 @@ namespace IntegrationTests
 		[Fact]
 		public void Transform_ShouldTransformCorrectly()
 		{
-			const string fileName = "appsettings.json";
-			var destination = this.GetOutputPath(fileName);
-			var source = this.GetResourcePath(fileName);
+			var destination = this.GetOutputPath($"appsettings{GetUniqueSuffix()}.json");
+			var source = this.GetResourcePath("appsettings.json");
 			var transformation = this.GetResourcePath("appsettings.Transformation.json");
 
 			this._fixture.JsonTransformer.Transform(destination, source, transformation);
